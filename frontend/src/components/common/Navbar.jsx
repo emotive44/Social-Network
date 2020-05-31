0dfeb367a03d13fa62c92191e7d9095c1e791ba1
marko streleshki <marko_streleshki_96@abv.bg>
2020-05-31 03:53:17 +0300

    implement toggle navbar for mobile view

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import './NavBar.scss';
 
 
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleBar = () => {
+    setToggle(!toggle);
+  }
+
   return (
     <nav>
       <Link to='' className='logo' />
-      <ul>
-        <li>Home</li>
-        <li>Users</li>
-        <li>Create Post</li>
-        <li>Login</li>
-        <li>Register</li>
+      <ul className={toggle ? 'show' : null}>
+        {!toggle && <i className='fas fa-bars' onClick={toggleBar} />}
+        {toggle && <i className='fas fa-times' onClick={toggleBar} />}
+        <li><NavLink to='/' activeClassName='active'>Home</NavLink></li>
+        <li><NavLink to=''>Users</NavLink></li>
+        <li><NavLink to=''>Posts</NavLink></li>
+        <li><NavLink to=''>Create Post</NavLink></li>
+        <li><NavLink to=''>Register</NavLink></li>
+        <li><NavLink to=''>Login</NavLink></li>
       </ul>
 
-      <p className="user">
+      <p className='user'>
         <span>Hello User</span>
-        <Link to='' className="user-image" />
+        <Link to='' className='user-image' />
       </p>
     </nav>
   );
